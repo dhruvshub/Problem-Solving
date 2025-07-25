dbab297673af044a9f1fff06fa4e6f2edb0c8c6e
dhruvyadav89300 <dhruv100.yadav.official@gmail.com>
2025-07-25 07:51:09 +0530

    added problem 1

---
+++ Project Euler/1. Multiples of 3 or 5.cpp
@@ -0,0 +1,22 @@
+// Problem Link: https://projecteuler.net/problem=1
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int sum = 0;
+
+    for (int i=0; i<1000; i++) {
+        if (i%3 == 0 && i%5==0) {
+            sum+=i;
+        }
+        else if (i%3 == 0) {
+            sum+=i;
+        }
+        else if (i%5 == 0) {
+            sum+=i;
+        }
+    }
+    cout << sum;
+}
---
+++ Project Euler/readme.md
@@ -0,0 +1,3 @@
+Platform link: https://projecteuler.net/
+
+I will use the filename as the problem name, the problem link would be commented out in the code itself.
