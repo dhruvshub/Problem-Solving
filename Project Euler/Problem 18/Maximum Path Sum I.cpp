5dc8a37951dbf6a7aa21f278220a152a8032f926
dhruvyadav89300 <dhruv100.yadav.official@gmail.com>
2025-08-29 08:28:44 +0530

    added problem 18

---
+++ Project Euler/Problem 18/Maximum Path Sum I.cpp
@@ -0,0 +1,38 @@
+// Problem Link: https://projecteuler.net/problem=18
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace  std;
+
+int main() {
+    vector<vector<int>> triangle = {
+        {75},
+        {95,64},
+        {17,47,82},
+        {18,35,87,10},
+        {20,4,82,47,65},
+        {19,1,23,75,3,34},
+        {88,2,77,73,7,63,67},
+        {99,65,4,28,6,16,70,92},
+        {41,41,26,56,83,40,80,70,33},
+        {41,48,72,33,47,32,37,16,94,29},
+        {53,71,44,65,25,43,91,52,97,51,14},
+        {70,11,33,28,77,73,17,78,39,68,17,57},
+        {91,71,52,38,17,14,91,43,58,50,27,29,48},
+        {63,66, 4,68,89,53,67,30,73,16,69,87,40,31},
+        { 4,62,98,27,23, 9,70,98,73,93,38,53,60, 4,23},
+    };
+    int best=0;
+    for (int i = triangle.size()-2; i >= 0; i--) {
+        vector<int> new_row;
+        for (int j = 0; j < triangle[i].size(); ++j) {
+            best = max(triangle[i][j]+triangle[i+1][j], triangle[i][j]+triangle[i+1][j+1]);
+            new_row.push_back(best);
+        }
+        triangle[i] = new_row;
+    }
+    cout << triangle[0][0];
+}
+
