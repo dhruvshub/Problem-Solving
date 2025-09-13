214ac0468fd6ca7b81909f86fee20a34419d7c2d
dhruvyadav89300 <dhruv100.yadav.official@gmail.com>
2025-09-13 07:38:49 +0530

    added Tower of Hanoi

--- Algorithms/Tower of Hanoi/Tower Of Hanoi.cpp
+++ Algorithms/Tower of Hanoi/Tower Of Hanoi.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-void tower_of_hanoi(int n, char source, char auxiliary, char destination) {
+int tower_of_hanoi(int n, char source, char auxiliary, char destination) {
+    static int moves = 0;
     if (n==1) {
         cout << "Move disk 1 from " << source << " to " << destination << endl;
+        moves+=1;
     }
     else if (n>0) {
         tower_of_hanoi(n-1, source, destination, auxiliary);
         cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+        moves+=1;
         tower_of_hanoi(n-1, auxiliary, source, destination);
     }
+    return moves;
 }
 
 int main() {
-    tower_of_hanoi(5, 'A', 'B', 'C');
+    int num_moves = tower_of_hanoi(3, 'A', 'B', 'C');
+    cout << endl << "Number of moves made: " << num_moves << endl;
 }
--- Algorithms/Tower of Hanoi/readme.md
+++ Algorithms/Tower of Hanoi/readme.md
@@ -11,5 +11,7 @@ to tower 2. After doing this tower 1 has only the largest disc left, move it to 
 This transferring of `n-1` discs is not necessarily done in a single step. Smaller steps may be required to complete the bigger
  step.
 
+Total number of moves required = 2<sup>n</sup>-1
+
 Time Complexity: O(2<sup>n</sup>) <br>
 Space Complexity: O(n)
